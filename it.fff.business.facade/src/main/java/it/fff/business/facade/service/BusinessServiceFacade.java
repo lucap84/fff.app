000a68f76bef0120e03649eb0387a40851fd938c
lucap84 <lucap84@gmail.com>
2015-11-04 17:41:32 +0100

    migliorie Upload File

--- it.fff.business.facade/src/main/java/it/fff/business/facade/service/BusinessServiceFacade.java
+++ it.fff.business.facade/src/main/java/it/fff/business/facade/service/BusinessServiceFacade.java
@@ -2,13 +2,27 @@ package it.fff.business.facade.service;
 
 import it.fff.business.common.dto.CreateUserDTO;
 import it.fff.business.common.dto.EventDTO;
+import it.fff.business.common.dto.ProfileImageDTO;
 import it.fff.business.common.dto.UserDTO;
+import it.fff.business.common.dto.WriteResultDTO;
+import it.fff.business.common.exception.ApplicationException;
+import it.fff.business.common.util.ErrorCodes;
 import it.fff.business.facade.exception.BusinessException;
 
-public interface BusinessServiceFacade {
+public abstract class BusinessServiceFacade {
 	
-	public EventDTO getEvent(int eventId) throws BusinessException;
+	public abstract EventDTO getEvent(int eventId) throws BusinessException;
 
-	public UserDTO createUser(CreateUserDTO createUserDTO) throws BusinessException;
+	public abstract UserDTO createUser(CreateUserDTO createUserDTO) throws BusinessException;
+
+	public abstract WriteResultDTO updateProfileImage(ProfileImageDTO dto) throws BusinessException;
+	
+	public void manageException(ApplicationException e, String errorCode) throws BusinessException{
+
+		BusinessException businessException = new BusinessException(e.getMessage(),e);
+		businessException.addErrorCodes(e.getErrorCodes());
+		businessException.addErrorCode(ErrorCodes.ERR_BUSIN_GETEVENT);
+		throw businessException;		
+	}
 
 }
--- it.fff.business.facade/src/main/java/it/fff/business/facade/service/impl/BusinessServiceFacadeImpl.java
+++ it.fff.business.facade/src/main/java/it/fff/business/facade/service/impl/BusinessServiceFacadeImpl.java
@@ -6,10 +6,13 @@ import org.apache.logging.log4j.Logger;
 import it.business.common.mapper.EventMapper;
 import it.business.common.mapper.UserMapper;
 import it.fff.business.common.bo.EventBO;
+import it.fff.business.common.bo.ProfileImageBO;
 import it.fff.business.common.bo.UserBO;
 import it.fff.business.common.dto.CreateUserDTO;
 import it.fff.business.common.dto.EventDTO;
+import it.fff.business.common.dto.ProfileImageDTO;
 import it.fff.business.common.dto.UserDTO;
+import it.fff.business.common.dto.WriteResultDTO;
 import it.fff.business.common.util.ErrorCodes;
 import it.fff.business.facade.exception.BusinessException;
 import it.fff.business.facade.service.BusinessServiceFacade;
@@ -18,7 +21,7 @@ import it.fff.business.service.UserBusinessService;
 import it.fff.business.util.BusinessServiceProvider;
 import it.fff.persistence.facade.exception.PersistenceException;
 
-public class BusinessServiceFacadeImpl implements BusinessServiceFacade{
+public class BusinessServiceFacadeImpl extends BusinessServiceFacade{
 	
 	private static final Logger logger = LogManager.getLogger(BusinessServiceFacadeImpl.class);
 	
@@ -30,16 +33,11 @@ public class BusinessServiceFacadeImpl implements BusinessServiceFacade{
 	public EventDTO getEvent(int eventId) throws BusinessException {
 		//recupero un bean prototype (non singleton) per avere una nuova istanza ed evitare problemi di concorrenza su operazione di business
 		EventBusinessService eventBusinessService = (EventBusinessService)BusinessServiceProvider.getBusinessService("eventBusinessService");
-		EventBO eventBO;
+		EventBO eventBO = null;
 		try {
 			eventBO = eventBusinessService.getEvent(eventId);
 		} catch (PersistenceException e) {
-			String errMess = "Errore da strato di persistenza. Message:"+e.getMessage();
-			logger.error(errMess);
-			BusinessException businessException = new BusinessException(errMess,e);
-			businessException.addErrorCodes(e.getErrorCodes());
-			businessException.addErrorCode(ErrorCodes.ERR_BUSIN_GETEVENT);
-			throw businessException;
+			super.manageException(e,ErrorCodes.ERR_BUSIN_GETEVENT);
 		}
 		if(eventBO!=null){
 			logger.debug("Event successfully retrieved by business layer");
@@ -61,12 +59,7 @@ public class BusinessServiceFacadeImpl implements BusinessServiceFacade{
 			userBO = mapper.mapCreateUserDto2Bo(createUserDTO);
 			userBO = userBusinessService.createUser(userBO);
 		} catch (PersistenceException e) {
-			String errMess = "Errore da strato di persistenza. Message:"+e.getMessage();
-			logger.error(errMess);
-			BusinessException businessException = new BusinessException(errMess,e);
-			businessException.addErrorCodes(e.getErrorCodes());
-			businessException.addErrorCode(ErrorCodes.ERR_BUSIN_CREATEUSER);
-			throw businessException;
+			super.manageException(e,ErrorCodes.ERR_BUSIN_CREATEUSER);			
 		}
 		if(userBO!=null){
 			logger.debug("User successfully retrieved by business layer");
@@ -77,6 +70,32 @@ public class BusinessServiceFacadeImpl implements BusinessServiceFacade{
 			logger.debug("Mapping bo2dto completed");
 		}
 		return dtoResult;
+	}
+
+	@Override
+	public WriteResultDTO updateProfileImage(ProfileImageDTO dto) throws BusinessException {
+		UserBusinessService userBusinessService = (UserBusinessService)BusinessServiceProvider.getBusinessService("userBusinessService");
+		WriteResultDTO resultDTO = new WriteResultDTO();
+		ProfileImageBO imgBO = null;
+		try {
+			UserMapper mapper = new UserMapper();
+			imgBO = mapper.mapProfileImageDto2Bo(dto);
+			imgBO = userBusinessService.updateProfileImage(imgBO);
+		} catch (PersistenceException e) {
+			super.manageException(e,ErrorCodes.ERR_BUSIN_CREATEUSER);
+		}
+		if(imgBO!=null){
+			logger.debug("Operation successful by business layer");
+		}
+		UserMapper mapper = new UserMapper();
+		ProfileImageDTO dtoResult = mapper.mapProfileImageBo2Dto(imgBO);
+		
+		if(dtoResult!=null){
+			resultDTO.setAffectedRecords(1);
+			resultDTO.setIdentifier(dtoResult.getImgHashCode());
+			logger.debug("Mapping bo2dto completed");
+		}
+		return resultDTO;
 	}
 
 
